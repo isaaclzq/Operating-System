c071180e761cf057d567e8089b87841c4558d7bf
isaaclzq <isaac.lzy@gmail.com>
2016-03-25 22:56:26 +0000

    reuse

--- hw3/mm_alloc.c
+++ hw3/mm_alloc.c
@@ -60,7 +60,7 @@ void *mm_malloc(size_t size) {
     // implementing first fit
     struct alloc_chunk* iter = chunk;
 	while (iter->free == 0){
-		if (iter->size >= size + meta_size){
+		if (iter->size + meta_size>= size + meta_size){
 			break;
 		} else if (NULL == iter->next){
 			break;
@@ -68,7 +68,7 @@ void *mm_malloc(size_t size) {
 		iter = iter->next;
 	}
 	printf("1\n");
-	if (iter->size >= size + meta_size && NULL != iter->next){
+	if (iter->size + meta_size >= size + meta_size){
 		if (iter->size + meta_size > size + 2 * meta_size){
 			printf("2\n");
 			reuse_and_alloc(iter, size);
